231541b97d3922d022f39dcda365c588fd51d023
XLNS-XYZ <30864336+XLNS-XYZ@users.noreply.github.com>
2018-01-22 20:01:47 +0000

    Create Fidelity.lua

---
+++ Rick/Fidelity.lua
@@ -0,0 +1,19 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Event = Instance.new("RemoteEvent", ReplicatedStorage)
+Event.Name = "TeamPlayer"
+
+local Reset = true
+ 
+local function onCreatePartFired(player,Team)
+	print ("Teamed "..(player.Name).." into "..Team.Name)
+	
+	if Reset then
+		player.Character.Humanoid.Health = 0	
+		wait()
+		player:LoadCharacter()
+	end
+	
+	player.Team = Team
+end
+ 
+Event.OnServerEvent:Connect(onCreatePartFired)
