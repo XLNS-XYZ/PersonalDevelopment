3da219043fb8f5121622d11610fa5dc975990215
XLNS-XYZ <30864336+XLNS-XYZ@users.noreply.github.com>
2018-02-10 22:33:46 +0000

    Update Secure_RankTabs.lua

--- Rick/Secure_RankTabs.lua
+++ Rick/Secure_RankTabs.lua
@@ -1,22 +1,19 @@
 local Players = game:GetService("Players")
- 
 function onPlayerAdded(player)
+	while not(player.Character) do wait(); end
 	
 	local function Interaction(touchedObject,bodyPart)
 		if (touchedObject:IsA("BasePart")) and (touchedObject:FindFirstChild("Tag")) then
 			player.Character.Torso:FindFirstChild("roblox").Texture = touchedObject.Tag.Texture
 		end
 	end
-	while (not player.Character) do wait() end
 	player.Character.Humanoid.Touched:connect(Interaction)
-	
-     print(player.Name .. " has entered the game")
 end
  
- game.Players.PlayerAdded:connect(function(player)
-	player.CharacterAdded:connect(onPlayerAdded)
- end)
---Call onPlayerAdded for each player already in the game
+game.Players.PlayerAdded:connect(function(player)
+	player.CharacterAdded:connect(onPlayerAdded(player))
+end)
+
 for _,player in pairs(Players:GetPlayers()) do
-     onPlayerAdded(player)
+	onPlayerAdded(player)
 end
