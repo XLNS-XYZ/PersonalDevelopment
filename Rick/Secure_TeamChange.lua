6c86e0aa1634d459c8a1001c8d0ebd1d7396bde7
XLNS-XYZ <30864336+XLNS-XYZ@users.noreply.github.com>
2018-02-17 22:23:23 +0000

    Update Secure_TeamChange.lua

--- Rick/Secure_TeamChange.lua
+++ Rick/Secure_TeamChange.lua
@@ -1,21 +1,21 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Event = Instance.new("RemoteEvent", ReplicatedStorage)
 Event.Name = "TeamPlayer"
-
+----------------------------------------------------------------
 local Reset = true
- 	
+----------------------------------------------------------------	
 local function onCreatePartFired(player,Team)
-	print ("Teamed "..(player.Name).." into "..Team.Name.. " successfully.")
 	
 	player.Team = Team
+	
+	print ("Teamed "..(player.Name).." into "..Team.Name.. " successfully.")
 	
 	if Reset then
 		player.Character.Humanoid.Health = 0	
 		wait()
 		player:LoadCharacter()
+		
 	end
-	
-	
 end
  
 Event.OnServerEvent:Connect(onCreatePartFired)
